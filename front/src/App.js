34c9e364f7bcc86becccbc590e078117c166915b
안태현 <shoe1541@naver.com>
2023-07-11 16:27:23 +0900

    api test

--- front/src/App.js
+++ front/src/App.js
@@ -7,19 +7,25 @@ function App() {
 
   const [text, setText] = useState([]);
 
-  axios.get("http://127.0.0.1:8000/api/test/")
-    .then((response) => {
-      setText([...response.data]);
-      console.log(text);
-      console.log(response.data);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+
   return (
-    <div>
-      안녕
-    </div>
+    <>
+      <div>
+        안녕
+      </div>
+      <button onClick={async () => {
+        await axios.get("http://127.0.0.1:8000/api/test/")
+          .then((response) => {
+            setText([...response.data]);
+            console.log(text);
+            console.log(response.data);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      }}>api 테스트</button>
+    </>
+
   );
 }
 
